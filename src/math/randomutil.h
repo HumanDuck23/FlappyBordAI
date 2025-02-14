157951f6e7aa5c15e43629b3f575ec63099f7b86
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2025-02-14 14:02:48 +0100

    implement randomness

---
+++ src/math/randomutil.h
@@ -0,0 +1,53 @@
+#ifndef RANDOMUTIL_H
+#define RANDOMUTIL_H
+
+#include <random>
+
+namespace randomutil {
+    class RandomFloatGen {
+    private:
+        static thread_local std::mt19937 gen;
+        std::uniform_real_distribution<float> distr;
+
+    public:
+        explicit RandomFloatGen(const float min, const float max) : distr(min, max) {}
+
+        float next() {
+            return distr(gen);
+        }
+
+        float operator()() {
+            return next();
+        }
+
+        static void seed(const unsigned int seed) {
+            gen.seed(seed);
+        }
+    };
+
+    class RandomIntGen {
+    private:
+        static thread_local std::mt19937 gen;
+        std::uniform_int_distribution<> distr;
+
+    public:
+        explicit RandomIntGen(const int min, const int max) : distr(min, max) {}
+
+        int next() {
+            return distr(gen);
+        }
+
+        int operator()() {
+            return next();
+        }
+
+        static void seed(const unsigned int seed) {
+            gen.seed(seed);
+        }
+    };
+
+    thread_local std::mt19937 RandomFloatGen::gen(std::random_device{}());
+    thread_local std::mt19937 RandomIntGen::gen(std::random_device{}());
+}
+
+#endif //RANDOMUTIL_H
